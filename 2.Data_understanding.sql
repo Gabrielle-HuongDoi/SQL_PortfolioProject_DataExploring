0097e9126fafaf7fa4f5d6161594031bdb85762e
HuongDoi <78661618+HuongDoi@users.noreply.github.com>
2022-02-17 21:11:30 +0100

    Update 2.Data_understanding.sql

--- 2.Data_understanding.sql
+++ 2.Data_understanding.sql
@@ -1,8 +1,8 @@
 ############################################################################################
-### DATA UNDERSTANDING
+-- DATA UNDERSTANDING
 ############################################################################################
 
-### ALL BREED_TYPE, BREED_GROUP AND CHARACTERS OF DOGS
+# ALL BREED_TYPE, BREED_GROUP AND CHARACTERS OF DOGS
 SELECT DISTINCT Breed_type
 FROM dogs;
 SELECT DISTINCT Breed_group
@@ -11,8 +11,9 @@ SELECT DISTINCT Dimension
 FROM dogs;
 # There are NULL variables in Breed_group and in Dimension field
 
+#--------------------------------------------------------------------------
 
-### CHECKING WHETHER THERE ARE DUPLICATES USER ACCOUNTS IN DOGS AND USERS TABLES
+# CHECKING WHETHER THERE ARE DUPLICATES USER ACCOUNTS IN DOGS AND USERS TABLES
 SELECT DISTINCT UserID, COUNT(UserID) as NumUsers
 FROM users
 GROUP BY UserID
@@ -31,13 +32,19 @@ GROUP BY DogID
 ORDER BY NumDogs DESC
 LIMIT 20;
 
-### COUNTRIES AND STATES IN WHICH USERS ATTENDING THE TESTS
+#----------------------------------------------------------------------------
+
+
+# COUNTRIES AND STATES IN WHICH USERS ATTENDING THE TESTS
 SELECT Country, State, COUNT(DISTINCT UserID) as NumUsers
 FROM users
 GROUP BY Country, State
 ORDER BY NumUsers DESC;
 
-### US AND NON-US CUSTOMERS
+#----------------------------------------------------------------------------
+
+
+# US AND NON-US CUSTOMERS
 SELECT IF(Country = 'US', 'US', IF(Country = 'N/A', 'Not Application', 'Non-US')) as User_residence, COUNT(UserID) as NumUsers
 FROM (SELECT DISTINCT UserID, Country
 		FROM users
@@ -45,7 +52,10 @@ FROM (SELECT DISTINCT UserID, Country
 GROUP BY User_residence
 ORDER BY NumUsers;
 
-### NUMBER OF UNIQUE DOGS DOING EACH TESTS
+#--------------------------------------------------------------------------------
+
+
+# NUMBER OF UNIQUE DOGS DOING EACH TESTS
 SELECT c.Test_name, COUNT(c.Created_at) as NumTests
 FROM dogs d JOIN complete_tests c ON d.DogID = c.DogID
 WHERE (d.Exclude IS NULL or d.Exclude = 0) AND c.Test_name IS NOT NULL
